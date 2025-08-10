92988c4a7e895929c898c62e030ed91cb9cc7112
Rajshekhar Bhagat <rajbhagat27889@gmail.com>
2025-08-10 12:49:59 +0530

    minor changes

--- src/socket/controllers/hive.controller.ts
+++ src/socket/controllers/hive.controller.ts
@@ -1,6 +1,6 @@
 import SubscriptionModel from "../models/subscription.model.js";
 
 export async function checkSubscription(hiveId: string, userId: string): Promise<boolean> {
-    const subscription = SubscriptionModel.findOne({user: userId, hive: hiveId})
+    const subscription = await SubscriptionModel.findOne({user: userId, hive: hiveId})
     return !!subscription;
 }
